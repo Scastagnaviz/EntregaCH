f08d860768e1996c676fa3e0b0471a2016c35dac
Santiago Castagnaviz <castagnaviz.santiago@gmail.com>
2022-08-27 17:52:56 -0300

    4

---
+++ test/mocha.js
@@ -0,0 +1,10 @@
+ const assert = require('supertest')('http://localhost:8080')
+ const expect = require('chai').expect
+
+ describe('test api producto',function(){
+    it('debeeria devolver un producto',function(){
+
+    })
+
+
+ })
